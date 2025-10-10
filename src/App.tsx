0292be03bfb0aaed4c37624aad9a288196afd282
ARTUMss <artmuzobny@gmail.com>
2025-10-10 06:48:49 +0300

    Fix5.4

--- src/App.tsx
+++ src/App.tsx
@@ -136,7 +136,7 @@ const LoginForm: React.FC<LoginFormProps> = ({ userEmail, setUserEmail, handleLo
   return (
     <div style={styles.loginContainer}>
       <img
-        src="https://i.ibb.co/0RJJYkHR/7cf9d1447585294415a64558ca6203333338s-ezgif-com-optimize.gi"
+        src="https://i.ibb.co/0RJJYkHR/7cf9d1447585294415a64558ca6203333338s-ezgif-com-optimize.gif"
         alt="background animation"
         style={styles.loginBackground}
       />
